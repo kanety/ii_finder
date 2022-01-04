4750b1b0011d5fd5a2d5ee95490b547f228a1197
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2022-01-04 13:18:20 +0900

    Tweak to_s for context

--- lib/ii_finder/context.rb
+++ lib/ii_finder/context.rb
@@ -2,5 +2,8 @@
 
 module IIFinder
   class Context < Coactive::Context
+    def to_s
+      "#<#{self.class} model=#{@_data[:model]} criteria=#{@_data[:criteria].to_s.truncate(300)}>"
+    end
   end
 end
