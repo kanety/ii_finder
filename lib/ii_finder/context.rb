4785bbd8f71b9b8fd17f217f493d71075f4cbafe
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2022-05-08 11:28:18 +0900

    Use coactive 0.3

--- lib/ii_finder/context.rb
+++ lib/ii_finder/context.rb
@@ -2,8 +2,5 @@
 
 module IIFinder
   class Context < Coactive::Context
-    def to_s
-      "#<#{self.class} model=#{@_data[:model]} criteria=#{@_data[:criteria].to_s.truncate(300)}>"
-    end
   end
 end
